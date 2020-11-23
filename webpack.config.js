d1970f7cc5591122b275840e6072346cff5d19c3
salty-byte <saltp40@gmail.com>
2020-11-23 17:03:38 +0900

    Update packages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
   },
   module: {
     rules: [
