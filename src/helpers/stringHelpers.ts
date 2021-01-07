3ca5e358ee6fb5014d01cb2505d8584321eb7529
salty-byte <saltp40@gmail.com>
2021-01-07 22:26:46 +0900

    Add comments to 'stringHelpers.ts'

--- src/helpers/stringHelpers.ts
+++ src/helpers/stringHelpers.ts
@@ -3,8 +3,8 @@
  *
  * ex) `abc&#x30b5;&#x30f3;&#x30d7;&#x30eb;` => `abcサンプル`
  *
- * @param {string} text A string containing character entity references
- * @return {string} A decoded string
+ * @param {string} text A string containing character entity references.
+ * @return {string} A decoded string.
  */
 export const decodeEntityReferences = (text: string): string => {
   return text.replace(/&#x(\w+?);/g, (_, char) => {
@@ -12,6 +12,15 @@ export const decodeEntityReferences = (text: string): string => {
   });
 };
 
+/**
+ * Get a value that matches the regular expression from the target string. If there is no matching value, it returns the substitute string.
+ *
+ * @param {string} text A search target.
+ * @param {RegExp} regExp Regular expression.
+ * @param {number} index Index of the array you want to retrieve from the results that match the regular expression. Default value is 0.
+ * @param {string} substitute If there is no matching the regular expression, returns this value. Default value is ''.
+ * @return {string} A string that matches the regular expression. If there is no matching value, it returns the substitute string.
+ */
 export const regExpOr = (
   target: string,
   regExp: RegExp,
