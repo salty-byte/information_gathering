6e54bee7bafd34948ac39d5a4e2ed24f15d263af
salty-byte <saltp40@gmail.com>
2021-12-11 22:19:46 +0900

    Fix the TheHackersNews to reflect changes in the link element

--- src/apps/theHackerNewsApp.ts
+++ src/apps/theHackerNewsApp.ts
@@ -4,12 +4,4 @@ export class TheHackerNewsApp extends BaseApp {
   constructor() {
     super('TheHackerNews', 'https://feeds.feedburner.com/TheHackersNews');
   }
-
-  protected getURLRegExp(): RegExp {
-    return /<feedburner:origLink>([\s\S]+?)<\/feedburner:origLink>/;
-  }
-
-  protected getDateRegExp(): RegExp {
-    return /<pubDate>([\s\S]+?)<\/pubDate>/;
-  }
 }
